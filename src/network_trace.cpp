0f4109198b19dce18fcb23cd03a7bdecee5d8729
ewilcox <ewilcox42@gmail.com>
2014-09-03 00:26:27 -0500

    added auto device detection

--- src/network_trace.cpp
+++ src/network_trace.cpp
@@ -11,7 +11,13 @@
 
 int main(int argc, char *argv[])
 	{
-		char *dev = argv[1];
+		char *dev, errbuf[PCAP_ERRBUF_SIZE];
+
+		dev = pcap_lookupdev(errbuf);
+		if (dev == NULL) {
+			fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+			return(2);
+		}
 		printf("Device: %s\n", dev);
-		return 0;
+		return(0);
 	}
