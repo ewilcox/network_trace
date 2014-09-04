fb8391ba00f0c335fe1db0ebc7444eb80557a476
ewilcox <ewilcox42@gmail.com>
2014-09-04 00:40:04 -0500

    Working with console run (no errbuf printed)

--- src/network_trace.c
+++ src/network_trace.c
@@ -13,14 +13,17 @@
 int main(int argc, char *argv[])
 {
 	char errbuf[PCAP_ERRBUF_SIZE];
-	FILE * fp;
-
+	pcap_t *handle;
 	if (argc < 2) {
 		printf("No pcap file to parse!\n");
 		exit(EXIT_FAILURE);
 	}
-	fp = fopen(argv[1],"r");
-	printf("File listed: %s\n", argv[1]);
-	//pcap_t *pcap_fopen_offline(*fp,*errbuf);
-	return(0);
+	char *file = argv[1];
+
+	printf("File: %s\n", file);
+	handle = pcap_open_offline(file,errbuf);
+	if (handle == NULL) printf("Error: %s\n",errbuf);
+
+	printf("end of program\n");
+	return 0;
 }
