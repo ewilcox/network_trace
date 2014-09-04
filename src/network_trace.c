5b4b46e0be3e2ecb7b229575a18e231cea432240
ewilcox <ewilcox42@gmail.com>
2014-09-03 23:56:48 -0500

    Switched to C project

--- src/network_trace.c
+++ src/network_trace.c
@@ -1,34 +1,26 @@
 //============================================================================
 // Name        : network_trace.cpp
 // Author      : Eric Wilcox
-// Version     :
+// Version     : 1.0
 // Copyright   : Advanced Network & Security course development project
-// Description : Hello World in C++, Ansi-style
+// Description : Pcap saved session reassymbly program in C
 //============================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-
 
 int main(int argc, char *argv[])
 {
-	string file;
-	pcap_t *dev;
-//	char *dev, errbuf[PCAP_ERRBUF_SIZE];
+	char errbuf[PCAP_ERRBUF_SIZE];
+	FILE * fp;
 
-	if (argc > 1) {
-		string file = argv[1];
-		cout << "File listed: " << file << endl;
-	} else {
-		cout << "No pcap file to parse!\n";
+	if (argc < 2) {
+		printf("No pcap file to parse!\n");
 		exit(EXIT_FAILURE);
 	}
-
+	fp = fopen(argv[1],"r");
+	printf("File listed: %s\n", argv[1]);
+	//pcap_t *pcap_fopen_offline(*fp,*errbuf);
 	return(0);
 }
