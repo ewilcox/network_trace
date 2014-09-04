0fc4c1fcc3e19aa77f719ea414a87ecd9908b44f
ewilcox <ewilcox42@gmail.com>
2014-09-03 22:35:23 -0500

    Inclusion of both trace files

--- src/network_trace.cpp
+++ src/network_trace.cpp
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <pcap.h>
 
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
+
 int main(int argc, char *argv[])
 	{
 		char *dev, errbuf[PCAP_ERRBUF_SIZE];
